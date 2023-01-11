a2436b09bc19478ba781e0aa579a9e6a33044a00
Tagawa Kodai <tkodai@c1r26s1.42tokyo.jp>
2023-01-11 12:42:57 +0900

    Add: gitignore

--- src/main.c
+++ src/main.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-int		main()
+#define BUFFER_SIZE	10000
+char	file_path[] = "./elf";
+char	buf[BUFFER_SIZE + 1];
+
+int		main(int argc, char *argv[])
 {
-	printf("ft_nm\n");
-	return 0;
+	int		fd;
+
+	if (argc != 2)
+		fd = open(file_path, O_RDWR);
+	else
+		fd = open(argv[1], O_RDWR);
+
+	int		ret = read(fd, buf, BUFFER_SIZE);
+
+	buf[ret] = '0';
+
+	for (int i = 0; i < 200; i++)
+	{
+		if (i % 8 == 0)
+			printf("%d: ", i * 8);
+		printf("[%x]", buf[i]);
+		if (i % 8 == 7)
+			printf("\n");
+	}
+
+	printf("\n");
+
+	for (int i = 0; i < 200; i++)
+	{
+		if (i % 8 == 0)
+			printf("%d: ", i * 8);
+		printf("[%d]", buf[i]);
+		if (i % 8 == 7)
+			printf("\n");
+	}
 }
