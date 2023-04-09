ae2ea44449d7fc7402a4a6b8d6f3cbd136c33c66
Tagawa Kodai <tkodai@c1r7s3.42tokyo.jp>
2023-04-09 20:37:14 +0900

    Add: read parse

--- includes/ft_nm.h
+++ includes/ft_nm.h
@@ -18,111 +18,29 @@
 #include "elf.h"
 #include "libft.h"
 
-#define	NM_STATUS_0				0
-#define NM_STATUS_MALLOC_ERROR	1
+#define DEBUG_PRINT printf("file: %s, func: %s (%d)\n", __FILE__, __func__, __LINE__);
+#define DEFAULT_ARG_FILE "./parse_file/ELF/hello"
 
-enum option_enum {
-  OPTION_a,
-  OPTION_g,
-  OPTION_u,
-  OPTION_r,
-  OPTION_p
-};
+#define NM_STATUS_0		0
+#define NM_MALLOC_ERROR 1
 
-#define OPTION_BIT_a			(1 << OPTION_a)
-#define OPTION_BIT_g			(1 << OPTION_g)
-#define OPTION_BIT_u			(1 << OPTION_u)
-#define OPTION_BIT_r			(1 << OPTION_r)
-#define OPTION_BIT_p			(1 << OPTION_p)
-
-typedef struct	s_option
+typedef struct		s_option
 {
-	int		option_bit;
-	int		option_error;
+	int		flag_bit;
+	int		error;
 	int		scan_bit;
+}					t_option;
 
-}				t_option;
-
-typedef struct	s_elf_64
+typedef struct		s_ft_nm
 {
-	void		*head;
-	Elf64_Ehdr	*ehdr;		//ELF header
-	Elf64_Shdr	*shdr_head;	//section header head ptr
-	Elf64_Shdr	*shdr_header_table;
+	int		argc;
+	char	**argv;
+	int		status;
 
-	Elf64_Shdr	*shdr_symbol;
-	Elf64_Shdr	*shdr_strtab;
-	Elf64_Sym	*sym_head;
+	t_option		option;
+}					t_ft_nm;
 
-	char		*sh_name_tab_ptr;  //section header name table head ptr
-	char		*sym_name_tab_ptr; //symbol name table head ptr
+//read_option
+void	read_option(t_ft_nm *ft_nm);
 
-}				t_elf_64;
-
-typedef struct	s_ft_nm
-{
-	//option
-	t_option	option;
-
-
-	int			status;
-	void		*head;
-
-	//type?
-	int			machine_type;
-	int			elf_format_size; //1 or 2 to signify 32- or 64-bit format
-
-	//read file
-	char		*file_name;
-	int			file_size;
-
-	
-	//
-	t_list_node	*file_list;
-
-	//symbol
-	t_list_node	*symbol_list;
-
-	/*
-	void		*head;
-	int			fd;
-	long long	file_size;
-
-	//option flag
-	
-	char		*shstrndx;
-	int			strtab_index;
-	int			symtab_index;
-
-	//
-	char		*prog_name_ptr;
-		*/
-
-}				t_ft_nm;
-
-typedef struct	s_sh_node_64
-{
-	t_ft_nm				*nm_ptr;
-	t_elf_64			*d;
-	Elf64_Sym			*symbol;
-	//unsigned long long	sh_offset;
-}				t_sh_node_64;
-
-
-void	read_option(t_ft_nm *n, int ar, char **argv);
-void	open_file(t_ft_nm *n);
-void	execute(t_ft_nm *n);
-void	nm_solve(t_ft_nm *n);
-int		is_valid_address(t_ft_nm *ft_nm, void *ptr);
-
-//scan 64
-void	scan_elf_header_64(t_ft_nm *ft_nm, t_elf_64 *ehdr);
-void	scan_section_header_64(t_ft_nm *f, t_elf_64 *e);
-
-//symbol
-void	display_symbol_node_64(void *content);
-int		sort_symbol_node_64(void *c1, void *c2);
-
-//error
-void	put_nm_error(char *f, char *m);
 #endif
--- src/read_option.c
+++ src/read_option.c
@@ -1,110 +1,41 @@
-#include "../includes/ft_nm.h"
-#include "../includes/elf.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   read_option.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkodai <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/09 20:21:33 by tkodai            #+#    #+#             */
+/*   Updated: 2023/04/09 20:36:01 by tkodai           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-//if exist duplicate option -> put error
-void	print_arg_error(t_ft_nm *ft_nm, t_option *op, int bit, const char *message)
-{
-	if (op->option_bit & bit)
-	{
-		ft_putstr("ft_nm: for the ");
-		ft_putstr(message);
-		ft_putstr(" option: may only occur zero or one times!\n");
-		op->option_error = 1;
-	}
-	else
-		op->option_bit |= bit;
+#include "ft_nm.h"
 
+void	scan_arg(t_ft_nm *ft_nm, char *arg)
+{DEBUG_PRINT;
 	(void)ft_nm;
+	(void)arg;
 }
 
-int		scan_arg_as_option(t_ft_nm *ft_nm, t_option *op, char *arg)
-{
-	int		hyphen_num = 0;
-	int		scan_success_num = 0;
+void	set_option(t_ft_nm *ft_nm)
+{DEBUG_PRINT;
+	int		index = 1;
 
-	while (*arg)
-	{
-		if (*arg != '-')
-			break ;
-		arg++;
-		hyphen_num++;
-	}
-	while (*arg)
-	{
-		if (*arg == 'a')
-			print_arg_error(ft_nm, op, OPTION_BIT_a, "-debug-syms");
-		else if (*arg == 'g')
-			op->option_bit |= OPTION_BIT_g;
-		else if (*arg == 'u')
-			print_arg_error(ft_nm, op, OPTION_BIT_u, "-undefined-only");
-		else if (*arg == 'r')
-			print_arg_error(ft_nm, op, OPTION_BIT_r, "-reverse-sort");
-		else if (*arg == 'p')
-			print_arg_error(ft_nm, op, OPTION_BIT_p, "-no-sort");
-		else
-			return 1;
-		arg++;
-		scan_success_num++;
-	}
-	if (scan_success_num == 0)
-		return 1;
-	return 0;
-}
-
-void	scan_arg_as_file(t_ft_nm *ft_nm, t_option *op, char *arg)
-{
-	printf("file: %s\n", arg);
-
-	ft_list_add_back_raw(&ft_nm->file_list, arg);
-
-	(void)op;
-
-}
-
-void	scan_arg(t_ft_nm *ft_nm, t_option *op, char *arg)
-{
-	char	*arg_head = arg;
-
-	if (*arg == '-')
-	{
-		if (scan_arg_as_option(ft_nm, op, arg) == 1)//error
-		{
-			op->option_error = 1;
-			ft_putstr("ft_nm: Unknown command line argument '");
-			ft_putstr(arg_head);
-			ft_putstr("'.\n");
-		}
-	}
-	else
-	{
-		scan_arg_as_file(ft_nm, op, arg);
-	}
-}
-
-void	set_option(t_ft_nm *ft_nm, int index, char *argv[])
-{
-	ft_nm->option.option_bit = 0;
-	ft_nm->option.option_error = 0;
+	//init option bits;
+	ft_nm->option.flag_bit = 0;
+	ft_nm->option.error = 0;
 	ft_nm->option.scan_bit = 0;
 
-	while (argv[index])
+	//loop args
+	while (ft_nm->argv[index])
 	{
-		scan_arg(ft_nm, &ft_nm->option, argv[index]);
+		scan_arg(ft_nm, ft_nm->argv[index]);
 		index++;
 	}
-	if (ft_nm->option.option_error == 1)
-		ft_nm->status = 1;
 }
 
-
-const char	default_input_file[] = "parse_files/ELF/hello";
-
-void	read_option(t_ft_nm *ft_nm, int argc, char *argv[])
-{
-	(void)argc;
-
-	set_option(ft_nm, 1, argv);
-
-	if (ft_nm->file_list == NULL)
-		ft_list_add_back_raw(&ft_nm->file_list, (char*)default_input_file);
+void	read_option(t_ft_nm *ft_nm)
+{DEBUG_PRINT;
+	set_option(ft_nm);
 }
