c30a2dac1a2d39e8b0012de91c0b9a7cbb9087ab
Tagawa Kodai <tkodai@c1r12s9.42tokyo.jp>
2023-04-13 01:38:55 +0900

    Add: stb

--- src/display_symbol_node.c
+++ src/display_symbol_node.c
@@ -1,36 +1,59 @@
 #include "ft_nm.h"
 #include "../includes/elf.h"
 
+void	debug_print_symbol_bind(int bind)
+{
+	//ft_putstr(" [");
+	if (STB_LOCAL == bind)
+		printf(" |%10s", "STB_LOCAL");
+	else if (STB_GLOBAL == bind)
+		printf(" |%10s", "STB_GLOBAL");
+	else if (STB_WEAK == bind)
+		printf(" |%10s", "STB_WEAK");
+	else if (STB_LOOS == bind)
+		printf(" |%10s", "STB_LOOS");
+	else if (STB_HIOS == bind)
+		printf(" |%10s", "STB_HIOS");
+	else if (STB_LOPROC == bind)
+		printf(" |%10s", "STB_LOPROC");
+	else if (STB_HIPROC == bind)
+		printf(" |%10s", "STB_HIPROC");
+	else
+		printf(" |%10s", "STT_*****");
+	
+	//ft_putstr("] ");
+}
+
 
 void	debug_print_symbol_type(int type)
 {
-	ft_putstr(" [");
+	//ft_putstr(" [");
 	if (STT_NOTYPE == type)
-		ft_putstr("STT_NOTYPE");
+		printf("%13s |", "STT_NOTYPE");
 	else if (STT_OBJECT == type)
-		ft_putstr("STT_OBJECT");
+		printf("%13s |", "STT_OBJECT");
 	else if (STT_FUNC == type)
-		ft_putstr("STT_FUNC");
+		printf("%13s |", "STT_FUNC");
 	else if (STT_SECTION == type)
-		ft_putstr("STT_SECTION");
+		printf("%13s |", "STT_SECTION");
 	else if (STT_FILE == type)
-		ft_putstr("STT_FILE");
+		printf("%13s |", "STT_FILE");
 	else if (STT_COMMON == type)
-		ft_putstr("STT_COMMON");
+		printf("%13s |", "STT_COMMON");
 	else if (STT_TLS == type)
-		ft_putstr("STT_TLS");
+		printf("%13s |", "STT_TLS");
 	else if (STT_LOOS == type)
-		ft_putstr("STT_LOOS");
+		printf("%13s |", "STT_LOOS");
 	else if (STT_HIOS == type)
-		ft_putstr("STT_HIOS");
+		printf("%13s |", "STT_HIOS");
 	else if (STT_LOPROC == type)
-		ft_putstr("STT_LOPROC");
+		printf("%13s |", "STT_LOPROC");
 	else if (STT_SPARC_REGISTER == type)
-		ft_putstr("STT_SPARC_REGISTER");
+		printf("%13s |", "STT_SPARC_REGISTER");
 	else if (STT_HIPROC == type)
-		ft_putstr("STT_HIPROC");
+		printf("%13s |", "STT_HIPROC");
 	
-	ft_putstr("] ");
+	//ft_putstr("] ");
 }
 
 void	debug_func(t_sym_node64 *node)
@@ -44,6 +67,11 @@ void	debug_func(t_sym_node64 *node)
 
 char	get_symbol(t_sym_node64 *node)
 {
+	int	bind = ELF64_ST_BIND(node->sym->st_info);
+
+	if (bind == STB_WEAK)
+		return 'w';
+
 	if (ft_strcmp(".text", node->shdr_name_ptr) == 0)	
 		return 't';
 	if (ft_strcmp(".bss", node->shdr_name_ptr) == 0)	
@@ -62,6 +90,14 @@ void	display_symbol_node_64(void *content)
 	//address => "0000000000001135"
 	ft_puthex(node->sym->st_value, 16, 0);
 
+	if (1)//debug
+	{
+		debug_print_symbol_bind(ELF64_ST_BIND(node->sym->st_info));
+		debug_print_symbol_type(ELF64_ST_TYPE(node->sym->st_info));
+		//debug_print_section_type(node->symbol);
+		fflush(stdout);
+	}
+
 	//space => ' '
 	ft_putchar(' ');
 
@@ -78,7 +114,8 @@ void	display_symbol_node_64(void *content)
 	if (1)
 	{
 		debug_func(node);
-		debug_print_symbol_type(ELF64_ST_TYPE(node->sym->st_info));
+		//debug_print_symbol_type(ELF64_ST_TYPE(node->sym->st_info));
+		//debug_print_symbol_bind(ELF64_ST_BIND(node->sym->st_info));
 	}
 
 	ft_putchar('\n');
